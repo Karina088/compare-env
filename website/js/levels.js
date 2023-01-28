654f58cd2ed4cbecd266f6d2af8951a67c3aeef1
kary_ser <morskya.life@gmail.com>
2023-01-28 18:31:47 +0300

    created a mini-game for comparing numbers and mathematical formulas

---
+++ website/js/levels.js
@@ -0,0 +1,80 @@
+// Функция для сохранения cookie
+function setCookie(name, value, expirationDelta) {
+  const d = new Date();
+  d.setTime(d.getTime() + (expirationDelta * 24 * 60 * 60 * 1000));
+  let expires = "expires=" + d.toUTCString();
+  document.cookie = name + "=" + value + ";" + expires + ";path=/";
+}
+
+// Функция для чтения конкретного значения cookie
+function getCookie(name) {
+  name = name + "=";
+  let cookies = document.cookie.split(';');
+  for (let i = 0; i < cookies.length; i++) {
+    let c = cookies[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+}
+
+let currentLevel // текущий уровень
+let metadata // список доступных уровней
+
+function init() {
+  let clientProgress = getCookie('clientProgress') // здесь хранятся ключи решённых пользователем задач
+  if (!clientProgress) { // если cookie ещё не задана, заводим пустой массив
+    setCookie('clientProgress', '[]', 365)
+    clientProgress = '[]'
+  }
+  clientProgress = JSON.parse(clientProgress)
+  for (let levelInfo of metadata.levels) { // среди уровней выбираем первый нерешённый
+    if (!clientProgress.includes(levelInfo.key)) {
+      console.log('init clientProgress = ' + JSON.stringify(clientProgress) + ' does not include levelInfo.key ' + levelInfo.key)
+      currentLevel = levelInfo
+      break
+    }
+  }
+  if (!currentLevel) { // если все уровни решены, выбираем первый
+    currentLevel = metadata.levels[0]
+  }
+  console.log('init clientProgress = ' + JSON.stringify(clientProgress))
+  console.log('init currentLevel = ' + JSON.stringify(currentLevel))
+  return currentLevel
+}
+
+function replaceChildren(source, target) {
+  while (target.firstChild) {
+    target.removeChild(target.lastChild);
+  }
+  target.append(...source.childNodes);
+}
+
+let answer
+
+function loadLevel(level) {
+  console.log('load level = ' + JSON.stringify(level))
+
+  fetch('/levels/' + level.key + '.xml')
+    .then(r => r.text())
+    .then(t => new DOMParser().parseFromString(t, 'text/xml'))
+    .then(xml => {
+      replaceChildren(xml.querySelector('left'), document.querySelector('#left'))
+      // replaceChildren(xml.querySelector('operator'), document.querySelector('#operator'))
+      answer = xml.querySelector('operator').innerHTML
+      replaceChildren(xml.querySelector('right'), document.querySelector('#right'))
+      // replaceChildren(xml.querySelector('explanation'), document.querySelector('#explanation'))
+    })
+
+    .then(() => true)
+}
+
+fetch('/levels/metadata.json')
+  .then(r => r.text())
+  .then(t => metadata = JSON.parse(t))
+  .then(init)
+  .then(loadLevel)
+  .then(() => true) // решение для ошибки: Uncaught (in promise) Error: A listener indicated an asynchronous ...
---
+++ website/js/main.js
@@ -0,0 +1,51 @@
+const operatorEl = document.querySelector('#middle')
+const leftEl = document.querySelector('#left')
+const rightEl = document.querySelector('#right')
+const container = document.querySelector('.container')
+const formulaEl = document.querySelector('.formula')
+
+let middleListener = () => {
+    operatorEl.innerHTML = `=`
+}
+
+operatorEl.addEventListener('mouseover',
+    middleListener)
+
+let leftListener = () => {
+    operatorEl.innerHTML = `&gt;`
+}
+
+leftEl.addEventListener('mouseover', leftListener)
+
+let rightListener = () => {
+    operatorEl.innerHTML = `&lt;`
+}
+
+rightEl.addEventListener('mouseover',
+    rightListener)
+
+const modal = document.querySelector('.modal')
+
+container.addEventListener('click', () => {
+    operatorEl.removeEventListener('mouseover', middleListener)
+    leftEl.removeEventListener('mouseover', leftListener)
+    rightEl.removeEventListener('mouseover', rightListener)
+
+    if (operatorEl.innerHTML === answer) {
+        modal.classList.add('modal-active', 'modal-close')
+        modal.innerHTML = 'Верно'
+    } else {
+        modal.classList.add('modal-active', 'modal-close')
+        modal.innerHTML = 'Неверно'
+    }
+})
+
+modal.addEventListener('click', () => {
+    if (operatorEl.innerHTML === answer) {
+        const cookieParse = JSON.parse(getCookie('clientProgress'))
+        cookieParse.push(currentLevel.key)
+        setCookie('clientProgress', JSON.stringify(cookieParse), 365)
+    }
+    location.reload()
+})
+
